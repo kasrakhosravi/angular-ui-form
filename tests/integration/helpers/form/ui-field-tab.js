62568e91810762092051d11cb03ffa10f26915f9
aramalipoor <aram.alipoor@gmail.com>
2015-08-02 11:03:16 +0430

    test(helpers): Implement field-tab and field-tree e2e helpers

--- tests/integration/helpers/form/ui-field-tab.js
+++ tests/integration/helpers/form/ui-field-tab.js
@@ -8,8 +8,32 @@ module.exports = {
     setData: function(pageObject, data) {
         return pageObject.buildChildrenPageObjects().then(function (children) {
             if (children.length > 0) {
-                // Only if it has children otherwise default helper tries to look for a vm.data (because it thinks its a simple field)
-                return DefaultHelper.setData(pageObject, data);
+                var headers = pageObject.getElement().all(by.xpath('./ui-field-row/div/div/ng-transclude/div/ul/li'));
+
+                return util.walkByPromise(children, function (child, index) {
+                    return child.getProperty().then(function (property) {
+                        return headers.get(index).getText().then(function (tabHeading) {
+                            var key = null;
+
+                            if (data[property]) {
+                                key = property;
+                            } else if (data[tabHeading]) {
+                                key = tabHeading;
+                            }
+
+                            if (key) {
+                                return headers.get(index).click().then(function () {
+                                    var chunk = {};
+                                        chunk[property] = data[key];
+
+                                    return DefaultHelper.setData(pageObject, chunk);
+                                });
+                            } else {
+                                return protractor.promise.when(true);
+                            }
+                        })
+                    });
+                });
             } else {
                 return protractor.promise.when(true);
             }
@@ -17,10 +41,42 @@ module.exports = {
     },
 
     getData: function(pageObject) {
+        var formElement = pageObject.getElement();
+
         return pageObject.buildChildrenPageObjects().then(function (children) {
+
+            // Only if it has children otherwise default helper tries to look for a vm.data (because it thinks its a simple field)
             if (children.length > 0) {
-                // Only if it has children otherwise default helper tries to look for a vm.data (because it thinks its a simple field)
-                return DefaultHelper.getData(pageObject);
+                var headers = formElement.all(by.xpath('./ui-field-row/div/div/ng-transclude/div/ul/li')),
+                    data = {};
+
+                return headers
+                    .map(function (element, index) {
+                        return {
+                            index: index,
+                            text: element.getText()
+                        };
+                    })
+                    .then(function (headersArray) {
+                        return util.walkByPromise(headersArray, function (header) {
+                            return headers.get(header.index).element(by.xpath('./a')).click().then(function () {
+                                return DefaultHelper.getData(
+                                    new FormPageObject(
+                                        formElement
+                                            .all(by.xpath('./ui-field-row/div/div/ng-transclude/div/div/div'))
+                                            .get(header.index)
+                                            .element(by.xpath('./ui-fieldset'))
+                                    )
+                                )
+                                .then(function (paneData) {
+                                    data[header.text] = paneData;
+                                });
+                            });
+                        });
+                    })
+                    .then(function () {
+                        return data;
+                    });
             } else {
                 return protractor.promise.when(null);
             }
