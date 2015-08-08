388df536e2d08dabbef14d8b98fbe0f11763deec
kasra <kasrakhosravi2@gmail.com>
2015-08-08 19:32:23 +0430

    fix(docs): Fixed [CS] in field choice example, Resolved issue with field choice controller

--- docs/dist/ui-form-docs.js
+++ docs/dist/ui-form-docs.js
@@ -84,6 +84,87 @@
 
     function configure($stateProvider) {
         $stateProvider
+            .state('ui.form.docs.examples.field_choice', {
+                url: '/field-choice',
+                templateUrl: 'ui-form/docs/examples/field-choice/template.html',
+                controller: 'ExampleFieldChoiceController',
+                controllerAs: 'vm',
+                ncyBreadcrumb: {
+                    label: 'Field Choice'
+                }
+            })
+        ;
+    }
+
+})(angular);
+
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('ui.form.docs')
+        .controller('ExampleFieldChoiceController', ExampleFieldChoiceController)
+    ;
+
+    function ExampleFieldChoiceController($timeout) {
+        var vm = this;
+
+        // Default bindings
+        vm.debug = false;
+
+        // Public methods
+        vm.save = save;
+        vm.saveWithErrors = saveWithErrors;
+
+        // -- Public Methods -- //
+
+        function save() {
+            if (vm.loading) {
+                return;
+            }
+
+            vm.loading = true;
+
+            // Mimicking async http call to save data.
+            $timeout(function () {
+                vm.loading = false;
+                alert('Data saved.');
+            }, 3000);
+        }
+
+        function saveWithErrors() {
+            if (vm.loading) {
+                return;
+            }
+
+            vm.loading = true;
+
+            // Mimicking async http call to save data, this time we pretend server has returned some validation errors.
+            $timeout(function () {
+                vm.loading = false;
+                vm.errors = {
+                    errors: ['This errors is about the whole form, not any specific field.'],
+                    children: {
+                        title: {
+                            errors: ['This is a validation error sent from server!']
+                        }
+                    }
+                };
+            }, 3000);
+        }
+    }
+
+})(angular);
+
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('ui.form.docs')
+        .config(configure);
+
+    function configure($stateProvider) {
+        $stateProvider
             .state('ui.form.docs.examples.simple_form', {
                 url: '/simple-form',
                 templateUrl: 'ui-form/docs/examples/simple-form/template.html',
---
+++ docs/src/examples/field-choice/config.js
@@ -0,0 +1,22 @@
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('ui.form.docs')
+        .config(configure);
+
+    function configure($stateProvider) {
+        $stateProvider
+            .state('ui.form.docs.examples.field_choice', {
+                url: '/field-choice',
+                templateUrl: 'ui-form/docs/examples/field-choice/template.html',
+                controller: 'ExampleFieldChoiceController',
+                controllerAs: 'vm',
+                ncyBreadcrumb: {
+                    label: 'Field Choice'
+                }
+            })
+        ;
+    }
+
+})(angular);
