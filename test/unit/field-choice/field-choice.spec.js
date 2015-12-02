b3a6d779240c84696279582d818880f6e79db075
kasra <kasrakhosravi2@gmail.com>
2015-12-02 15:59:34 +0330

    test(Choice): Add unit test when field choice is required, Refactor choice unit test file

--- test/unit/field-choice/field-choice.spec.js
+++ test/unit/field-choice/field-choice.spec.js
@@ -16,103 +16,150 @@ describe('Field Choice', function() {
         $rootScope = _$rootScope_;
     }));
 
-    describe('inlineOptions', function() {
-
-        it('should fill select options from provided key-valued inline options when it is not required', function () {
-            compileChoiceField(
-                'inline-options="inlineOptions"'
-            );
-
-            scope.$apply(function () {
-                scope.inlineOptions = {
-                    foo: 'bar',
-                    baz: 'ban'
-                };
-            });
-
-            expect(element.find('option').length).toBe(3);
-        });
-
-        it('should fill select options from provided associated-array of inline options when it is not required',
-            function () {
-                compileChoiceField(
-                    'inline-options="inlineOptions" label-property="label" value-property="value"'
-                );
-
-                scope.$apply(function () {
-                    scope.inlineOptions = [
-                        {label: 'foo', value: 'bar'},
-                        {label: 'baz', value: 'ban'}
-                    ];
-                });
-
-                expect(element.find('option').length).toBe(3);
-            }
+    // Inline Options
+    it('should fill select options from provided key-valued inline options', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions"'
         );
 
-        it('should fill select options from provided simple array of inline options when it is not required', function () {
-            compileChoiceField(
-                'inline-options="inlineOptions" label-property="label" value-property="value"'
-            );
-
-            scope.$apply(function () {
-                scope.inlineOptions = ['foo', 'bar'];
-            });
-
-            expect(element.find('option').length).toBe(3);
+        scope.$apply(function () {
+            scope.inlineOptions = {
+                foo: 'bar',
+                baz: 'ban'
+            };
         });
 
-        it('should have value of selected option', function () {
-            compileChoiceField(
-                'inline-options="inlineOptions" label-property="label" value-property="value"'
-            );
-
-            scope.$apply(function () {
-                scope.inlineOptions = [
-                    {label: 'foo', value: 'bar'},
-                    {label: 'baz', value: 'ban'}
-                ];
-            });
-
-            scope.$apply(function () {
-                scope.data = 'bar';
-            });
-
-            expect(element.find('select').val()).toBe('0');
-
-            scope.$apply(function () {
-                scope.data = 'ban';
-            });
-
-            expect(element.find('select').val()).toBe('1');
-        });
-
+        expect(element.find('option').length).toBe(3);
     });
 
-
-    describe('required', function() {
-
-        it('should have an extra field with null value when it is a combo box with required scope set to true',
-            function() {
-                compileChoiceField(
-                    'inline-options="inlineOptions" required="required"'
-                );
-
-                scope.$apply(function () {
-                    scope.inlineOptions = {
-                        foo: 'bar',
-                        baz: 'ban'
-                    };
-                });
-
-                expect(element.find('option').length).toBe(3);
-
-                scope.$apply(function () {
-                    scope.required = true;
-                });
-
-                expect(element.find('option').length).toBe(2);
-            }
+    it('should fill select options from provided simple array of inline options', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions"'
         );
+
+        scope.$apply(function () {
+            scope.inlineOptions = ['foo', 'bar'];
+        });
+
+        expect(element.find('option').length).toBe(3);
+    });
+
+    it('should fill select options from provided associated-array of inline options', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions" label-property="label" value-property="value"'
+        );
+
+        scope.$apply(function () {
+            scope.inlineOptions = [
+                {label: 'foo', value: 'bar'},
+                {label: 'baz', value: 'ban'}
+            ];
+        });
+
+        expect(element.find('option').length).toBe(3);
+        });
+
+    it('should have value of selected option', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions" label-property="label" value-property="value"'
+        );
+
+        scope.$apply(function () {
+            scope.inlineOptions = [
+                {label: 'foo', value: 'bar'},
+                {label: 'baz', value: 'ban'}
+            ];
+        });
+
+        scope.$apply(function () {
+            scope.data = 'bar';
+        });
+
+        expect(element.find('select').val()).toBe('0');
+
+        scope.$apply(function () {
+            scope.data = 'ban';
+        });
+
+        expect(element.find('select').val()).toBe('1');
+    });
+
+    // Inline Options, Required
+    it('should have an extra field when it is a combo box (single and not expanded)', function() {
+        compileChoiceField(
+            'inline-options="inlineOptions" required="required"'
+        );
+
+        scope.$apply(function () {
+            scope.inlineOptions = {
+                foo: 'bar',
+                baz: 'ban'
+            };
+        });
+
+        expect(element.find('option').length).toBe(3);
+
+        scope.$apply(function () {
+            scope.required = true;
+        });
+
+        expect(element.find('option').length).toBe(2);
+    });
+
+    it('should select the first item when vm.data is undefined', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions" label-property="label" value-property="value" required="true"'
+        );
+
+        scope.$apply(function () {
+            scope.inlineOptions = [
+                {label: 'foo', value: 'bar'},
+                {label: 'baz', value: 'ban'}
+            ];
+        });
+
+        scope.$apply(function () {
+            scope.data = undefined;
+        });
+
+        expect(element.find('select').val()).toBe('0');
+    });
+
+    it('should select the first item when vm.data is null', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions" label-property="label" value-property="value" required="true"'
+        );
+
+        scope.$apply(function () {
+            scope.inlineOptions = [
+                {label: 'foo', value: 'bar'},
+                {label: 'baz', value: 'ban'}
+            ];
+        });
+
+        scope.$apply(function () {
+            scope.data = null;
+        });
+
+        expect(element.find('select').val()).toBe('0');
+    });
+
+    it('should select the first item when vm.data is an empty string', function () {
+        compileChoiceField(
+            'inline-options="inlineOptions" label-property="label" value-property="value" required="true"'
+        );
+
+        scope.$apply(function () {
+            scope.inlineOptions = [
+                {label: 'foo', value: 'bar'},
+                {label: 'baz', value: 'ban'}
+            ];
+        });
+
+        scope.$apply(function () {
+            scope.data = "";
+        });
+
+        expect(element.find('select').val()).toBe('0');
     });
 });
